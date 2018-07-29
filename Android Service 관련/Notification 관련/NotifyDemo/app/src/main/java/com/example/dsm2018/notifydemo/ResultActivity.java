f9321605b001e0e9a7f6e8657d0e2d8acac19b8e
jinusong <sugwer7464@gmail.com>
2018-07-29 22:40:16 +0900

    SImple Notification

---
+++ Android Service 관련/Notification 관련/NotifyDemo/app/src/main/java/com/example/dsm2018/notifydemo/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.example.dsm2018.notifydemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify_demo);
+    }
+}
