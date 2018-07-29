cfc4ef721b377302c9e35735c1d51005d273153d
jinusong <sugwer7464@gmail.com>
2018-07-29 16:05:09 +0900

    Broadcast Intent and Broadcast_Receiver

---
+++ Activity 관련/Intent 관련/MYBroadcastReceiver/app/src/main/java/com/example/dsm2018/mybroadcastreceiver/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.dsm2018.mybroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String message = "Broadcast intent detected "
+                + intent.getAction();
+        Toast.makeText(context, message,
+                Toast.LENGTH_LONG).show();
+    }
+}
---
+++ Activity 관련/Intent 관련/SendBroadcast/app/src/main/java/com/example/dsm2018/sendbroadcast/SendBroadcastActivity.java
@@ -0,0 +1,22 @@
+package com.example.dsm2018.sendbroadcast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SendBroadcastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_broadcast);
+    }
+
+    public void broadcastIntent (View view) {
+        Intent intent = new Intent();
+        intent.setAction("com.example.dsm2018.sendbroadcast");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+    }
+}
