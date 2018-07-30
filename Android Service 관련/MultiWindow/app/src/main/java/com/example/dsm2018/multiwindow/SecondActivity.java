d6057b01f5ef182c94ccf9c8799a160eb26bb83b
jinusong <sugwer7464@gmail.com>
2018-07-30 23:14:30 +0900

    Multi Window

---
+++ Android Service 관련/MultiWindow/app/src/main/java/com/example/dsm2018/multiwindow/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.dsm2018.multiwindow;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
