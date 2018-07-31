5439a8bd42cac5e3bf81c5d9ba64d489705b8d41
jinusong <sugwer7464@gmail.com>
2018-07-31 20:54:15 +0900

    Simple Database Communication

---
+++ Android Database 관련/Database/app/src/main/java/com/example/dsm2018/database/MyDBHandler.java
@@ -0,0 +1,94 @@
+package com.example.dsm2018.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDBHandler extends SQLiteOpenHelper{
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "productDB.db";
+    private static final String TABLE_PRODUCTS = "products";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productname";
+    public static final String COLUMN_QUANTITY = "quantity";
+
+    public MyDBHandler(Context context, String name,
+                       SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    public void addProduct(Product product) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCTNAME, product.getProductName());
+        values.put(COLUMN_QUANTITY, product.get_Quantity());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert(TABLE_PRODUCTS, null, values);
+        db.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_PRODUCTS_TABLE = "CREATE TABLE " +
+                TABLE_PRODUCTS + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_PRODUCTNAME
+                + " TEXT," + COLUMN_QUANTITY + " INTEGER" + ")";
+        sqLiteDatabase.execSQL(CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(sqLiteDatabase);
+    }
+
+    public Product findProduct(String productname) {
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE " +
+                COLUMN_PRODUCTNAME+ " =  \"" + productname +"\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        Product product = new Product();
+
+        if(cursor.moveToFirst()) {
+            cursor.moveToFirst();
+            product.setID(Integer.parseInt(cursor.getString(0)));
+            product.setProductName(cursor.getString(1));
+            product.setQuantity(Integer.parseInt(cursor.getString(2)));
+            cursor.close();
+        } else {
+            product = null;
+        }
+        db.close();
+        return product;
+    }
+    public boolean deleteProduct(String productname) {
+
+        boolean result = false;
+
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE" +
+                COLUMN_PRODUCTNAME + " =  \"" + productname + "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        Product product = new Product();
+
+        if(cursor.moveToFirst()) {
+            product.setID(Integer.parseInt(cursor.getString(0)));
+            db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?",
+                    new String[] { String.valueOf(product.getID()) });
+            cursor.close();
+            result = true;
+        }
+        db.close();
+        return result;
+    }
+}
---
+++ Android Database 관련/Database/app/src/main/java/com/example/dsm2018/database/Product.java
@@ -0,0 +1,45 @@
+package com.example.dsm2018.database;
+
+public class Product {
+    private int _id;
+    private String _productname;
+    private int _quantity;
+
+    public Product() {
+    }
+
+    public Product(int id, String _productname, int _quantity) {
+        this._id = id;
+        this._productname = _productname;
+        this._quantity = _quantity;
+    }
+
+    public Product(String productname, int quantity) {
+        this._productname = productname;
+        this._quantity = quantity;
+    }
+
+    public void setID(int id) {
+        this._id = id;
+    }
+
+    public int getID() {
+        return this._id;
+    }
+
+    public void setProductName(String productname) {
+        this._productname = productname;
+    }
+
+    public String getProductName() {
+        return this._productname;
+    }
+
+    public void setQuantity(int quantity) {
+        this._quantity = quantity;
+    }
+
+    public int get_Quantity() {
+        return this._quantity;
+    }
+}
