d8a8d2d34592b0948e541bca097d1e407c9e1ffa
jinusong <sugwer7464@gmail.com>
2018-08-09 23:43:02 +0900

    New

---
+++ app/src/main/java/com/example/dsm2018/practiceproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dsm2018.practiceproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
